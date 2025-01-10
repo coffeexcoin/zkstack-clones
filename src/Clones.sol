c0427c122e81f4875a893053cbe104f87d7a9e9e
Coffee☕️ <coffee@coffeebot.dev>
2025-01-10 14:06:43 -0500

    update natspec

--- src/Clones.sol
+++ src/Clones.sol
@@ -11,15 +11,12 @@ import {Errors} from "./Errors.sol";
  * deploying minimal proxy contracts, also known as "clones".
  *
  * > To simply and cheaply clone contract functionality in an immutable way, this standard specifies
- * > a minimal bytecode implementation that delegates all calls to a known, fixed address.
+ * > a minimal implementation that delegates all calls to a known, fixed address.
  *
  * The library includes functions to deploy a proxy using either `create` (traditional deployment) or `create2`
- * (salted deterministic deployment). It also includes functions to predict the addresses of clones deployed using the
- * deterministic method.
+ * (salted deterministic deployment).
  */
 library Clones {
-    error CloneArgumentsTooLong();
-
     /**
      * @dev Deploys and returns the address of a clone that mimics the behaviour of `implementation`.
      *
